030b2130c27bf419e051e21f78a8740a4338af28
Jake Good <jake@developstuff.com>
2011-06-17 15:00:47 -0500

    Runs

--- lib/servloko.rb
+++ lib/servloko.rb
@@ -5,20 +5,20 @@ module Servloko
   
   def self.start(file,port,daemonize)
     
-    rackup = Tempfile.new('servloko.ru')
+    rackup = Tempfile.new(['servloko','.ru'])
     
     begin
        rackup.write "app = proc do |env|\n"
        rackup.write "  body = [`ruby #{file}`.chomp]\n"
-       rackup.write "  [200,{'Content-Type => 'text/html'},body]\n"
+       rackup.write "  [200,{'Content-Type' => 'text/html'},body]\n"
        rackup.write "end\n"
        rackup.write "run app\n"
        rackup.flush
 
        if daemonize
-         `nohup thin -R #{rackup} -p #{port} &`
+         `nohup thin -R #{rackup.path} -p #{port} &`
        else
-         `thin -R #{rackup} -p {port}`
+         puts `thin start -R #{rackup.path} -p #{port}`
        end
     ensure
        rackup.close
