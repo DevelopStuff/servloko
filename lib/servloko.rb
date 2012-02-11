488cfba7f00974540d1e6b255197d3fe1f21015b
Jake Good <jake@developstuff.com>
2012-02-11 12:31:22 -0600

    Logger information

--- lib/servloko.rb
+++ lib/servloko.rb
@@ -1,13 +1,22 @@
 require 'tempfile'
+require 'pty'
 require 'servloko/version'
 
 module Servloko
   
-  def self.start(file,port,daemonize)
+  def self.start(opts = {})
+    file = opts[:file]
+    port = opts[:port] || 3000
+    daemonize = opts[:daemonize] || false
+    pid_file = opts[:pid_file] if daemonize
+    
     puts "Servloko v#{Servloko::VERSION} booting on port #{port}"
     puts "------------------------------------------------------"
     rackup = Tempfile.new(['servloko','.ru'])
     begin
+      if !daemonize
+        rackup.write "use Rack::CommonLogger\n"
+      end
       rackup.write "app = proc do |env|\n"
       rackup.write "  req = Rack::Request.new(env)\n"
       rackup.write "  body = [`ruby #{file} \"\#\{req.query_string\}\"`.chomp.gsub('\r\n','<br/>')]\n"
@@ -17,15 +26,19 @@ module Servloko
       rackup.flush
 
       if daemonize
-        puts `nohup thin start -R #{rackup.path} -p #{port} &`
+        pid_file ||= File.join(Dir::tmpdir,"servloko.pid")
+        puts "starting thin in daemon mode."
+        puts "to stop: cat #{pid_file} | xargs kill"
+        puts `thin start -R #{rackup.path} -p #{port} -d -P #{pid_file}`
       else
-        puts `thin start -R #{rackup.path} -p #{port}`
+        PTY.spawn "thin start -R #{rackup.path} -p #{port}" do |r,w,p|
+          loop { puts r.gets }
+        end
       end
     rescue SignalException => e
-      puts
+      puts "shutting down..."
     ensure
       puts "------------------------------------------------------"
-      puts "shutting down..."
       rackup.close
       rackup.unlink
     end
