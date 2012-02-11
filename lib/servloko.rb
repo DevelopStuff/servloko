aa598b9b9b0dfaa797a2109955584ade476b7323
Jake Good <jake@developstuff.com>
2012-02-11 11:48:08 -0600

    Some tidying up

--- lib/servloko.rb
+++ lib/servloko.rb
@@ -4,25 +4,30 @@ require 'servloko/version'
 module Servloko
   
   def self.start(file,port,daemonize)
-    
+    puts "Servloko v#{Servloko::VERSION} booting on port #{port}"
+    puts "------------------------------------------------------"
     rackup = Tempfile.new(['servloko','.ru'])
-    
     begin
-       rackup.write "app = proc do |env|\n"
-       rackup.write "  body = [`ruby #{file}`.chomp.gsub('\r\n','<br/>')]\n"
-       rackup.write "  [200,{'Content-Type' => 'text/html'},body]\n"
-       rackup.write "end\n"
-       rackup.write "run app\n"
-       rackup.flush
+      rackup.write "app = proc do |env|\n"
+      rackup.write "  req = Rack::Request.new(env)\n"
+      rackup.write "  body = [`ruby #{file} \"\#\{req.query_string\}\"`.chomp.gsub('\r\n','<br/>')]\n"
+      rackup.write "  [200,{'Content-Type' => 'text/html'},body]\n"
+      rackup.write "end\n"
+      rackup.write "run app\n"
+      rackup.flush
 
-       if daemonize
-         puts `nohup thin start -R #{rackup.path} -p #{port} &`
-       else
-         puts `thin start -R #{rackup.path} -p #{port}`
-       end
+      if daemonize
+        puts `nohup thin start -R #{rackup.path} -p #{port} &`
+      else
+        puts `thin start -R #{rackup.path} -p #{port}`
+      end
+    rescue SignalException => e
+      puts
     ensure
-       rackup.close
-       rackup.unlink
+      puts "------------------------------------------------------"
+      puts "shutting down..."
+      rackup.close
+      rackup.unlink
     end
   end
   
